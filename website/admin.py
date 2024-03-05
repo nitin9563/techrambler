95e09e6dbcfe7a21f5ee7916055557f3799fc1c0
palak-kalra <palakkalra604@gmail.com>
2024-03-05 16:06:02 +0530

    changes

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from website.models import *
 
 # Register your models here.
+admin.site.register(Content)
+admin.site.register(Category)
+admin.site.register(Playlist)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from datetime import date
+
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name=models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.category_name
+
+class Playlist(models.Model):
+    playlist_name=models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.playlist_name
+
+class Content(models.Model):
+    title=models.CharField(max_length=200,blank=False)
+    description=models.TextField()
+    date=models.DateField(default=date.today())
+    thumbnail=models.FileField()
+    video_link=models.CharField(max_length=1000)
+    playlist=models.ForeignKey(Playlist,on_delete=models.SET_NULL, null=True)
+    category=models.ForeignKey(Category,on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.id} - video - Title: {self.title}"
+    
