aa64e6178a9db7c86c5fef32dff85a05395638ba
nitin9563 <=>
2024-03-13 15:10:00 +0530

    blabka

--- static/script.js
+++ static/script.js
@@ -2,18 +2,13 @@
 
 function toggleActive(x){
     let buttons = document.querySelectorAll(".nc-a");
-    let arr = document.querySelectorAll(".all-posts");
 
-    console.log(arr);
-    buttons.forEach(e => {
-        e.classList.remove("nc-a-active");
-    });
+    if (x==4){
+        buttons[x-1].classList.add("nc-a-active");
+        buttons.forEach(e => {
+            e.classList.remove("nc-a-active");
+        });
+    }
 
-    arr.forEach(e => {
-        e.classList.remove("all-posts-active");
-        console.log(e,"done")
-    });
-
-    buttons[x-1].classList.add("nc-a-active");
-    arr[x-1].classList.add("all-posts-active");
+    
 }
--- website/views.py
+++ website/views.py
@@ -4,11 +4,40 @@ from random import choice
 
 # Create your views here.
 def index(request):
-    videos = Content.objects.all()
-    playlist = Playlist.objects.all()
-    category = Category.objects.all()
+    videos_data = Content.objects.all()
 
-    return render(request,"index.html",{"video":videos,"playlist":playlist,"category":category})
+    query = request.GET
+    specific = ""
+
+    data_role = "main"
+
+    data = videos_data
+
+    if "playlist" in query:
+        playlist_data = Playlist.objects.all()
+        data = playlist_data
+        data_role = "play"
+
+    if "category" in query:
+        category_data = Category.objects.all()
+        data = category_data
+        data_role = "cat"
+
+    if "category_name" in query:
+        obj = Category.objects.get(category_name = query["category_name"])
+        videos_specific = Content.objects.filter(category = obj)
+        specific = f"category > {query['category_name']}"
+        data = videos_specific
+        data_role = "main"
+
+    if "playlist_name" in query:
+        obj = Playlist.objects.get(playlist_name = query["playlist_name"])
+        videos_specific = Content.objects.filter(playlist = obj)
+        specific = f"Playlist > {query['playlist_name']}"
+        data = videos_specific
+        data_role = "main"
+    
+    return render(request,"index.html",{"data" : data,"specific" : specific, "data_role" :data_role})
 
 def dataadd(request):
 
@@ -19,8 +48,11 @@ def dataadd(request):
     # for i in c:
     #     Content.objects.filter(id=i.id).delete()
 
-    for i in range(20):
-        x = Content(title = "Python Magic: Create Pattern (6) with Ease. ",description = "Unlock the magic of Python with this tutorial on creating star patterns effortlessly.#pythonpattern #python", playlist=choice(b),category = choice(a),thumbnail = "thumbnails\Thumbnail-dj-2.jpg")
+    for i in range(50):
+        playlist=choice(b)
+        category = choice(a)
+
+        x = Content(title = f"playlist : {playlist} and with category : {category}",description = "Unlock the magic of Python with this tutorial on creating star patterns effortlessly.#pythonpattern #python", playlist=playlist,category = category,thumbnail = "thumbnails\Thumbnail-dj-2.jpg")
         x.save()
         
     return HttpResponse("done check")
